e7d4bea1f7d01cbc07e5bc5c17c5b01f1c3a85f1
tasniaarhamarham <tasniaarhamarham@gmail.com>
2025-10-14 09:11:14 +0600

    try to nodemailer

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import middleware from "./middlewares"
 import z from "zod"
 import route from "./routes"
 import globalErrorHandler from "./middlewares/globalErrorHandler"
-  
+import nodemailer from "nodemailer"
   dotenv.config({path: './.env',});
   
   export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
@@ -47,7 +47,28 @@ app.post('/', async (req, res) => {
 });
   
   
+  app.post('/send',async(req,res)=>{
+    const transporter = nodemailer.createTransport({
+      host: "admin@gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "jude.boyer14@ethereal.email",
+        pass: "cvhXGfpt78dvJcVB8g",
+      },
+    });
     
+    // Wrap in an async IIFE so we can use await.
+  
+      const info = await transporter.sendMail({
+        from: '"Maddison Foo Koch" <jude.boyer14@ethereal.email>',
+        to: "sharintasnia1@gmail.com",
+        subject: "Hello ✔",
+        text: "Hello world?", // plain‑text body
+        html: "<b>Hello world?</b>", // HTML body
+      });
+    res.status(200).send(info);
+  })
   app.get("*", (req, res) => {
     res.status(404).json({
       success: false,
